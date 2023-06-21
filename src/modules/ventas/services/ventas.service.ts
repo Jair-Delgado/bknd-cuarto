308cb6db5dad10d83cca0c905f47b80f68b0b034
Jair Delgado <jar.delgado@yavirac.edu.ec>
2023-06-21 14:52:24 -0500

    Pagination

--- src/modules/ventas/services/ventas.service.ts
+++ src/modules/ventas/services/ventas.service.ts
@@ -1,8 +1,8 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { RepositoryEnum } from 'src/shared/enums';
 import { ProductEntity } from '../entities';
-import { Repository, ReturnDocument } from 'typeorm';
-import { CreateProductDto, FilterProductDto, ReadProductDto } from '../dtos';
+import { FindOptions, FindOptionsWhere, ILike, Repository, ReturnDocument } from 'typeorm';
+import { CreateProductDto, FilterProductDto, ReadProductDto, UpdateProductDto } from '../dtos';
 
 @Injectable()
 export class VentasService {
@@ -10,20 +10,20 @@ constructor(
     @Inject(RepositoryEnum.PRODUCT_REPOSITORY)
     private repository:Repository<ProductEntity>
 )
-async create(payload: CreateProductDto): Promise<ServiceResponseHttpModel>{
+async create(payload: CreateProductDto){
     const newProduct = this.repository.create(payload);
     const productCreated = await this.repository.save(newProduct);
 
     return {data: plainToInstance(ReadProductDto, productCreated)}
 }
-async catalogue(): Promise<ServiceResponseHttpModel>{
+async catalogue(){
     const response = this.repository.findAndCount({take: 1000});
     return {data: response[0],
             pagination: {totalItems: response[1], limit: 10}
         };
 }
 
-async findAll(params? : FilterProductDto):Promise<ServiceResponseHttpModel>{
+async findAll(params? : FilterProductDto){
     //😁
     if (params?.limit > 0 && params? >=0) {
         return this.paginateAndFilter(params);
@@ -34,19 +34,57 @@ async findAll(params? : FilterProductDto):Promise<ServiceResponseHttpModel>{
     
     return {
         data: plainToInstance(ReadProductDto, response[0]),
-        pagination: {totalItems: response[1],limit 10},
+        pagination: {totalItems: response[1],limit: 10},
     }
 
 }
-async findOne(id:string):Promise<ServiceResponseHttpModel>{
+async findOne(id:string){
     //😁
-    const response = this.repository.findOne({
+    const response = await this.repository.findOne({
         where: {id},
     });
     if (!response) {
         throw new NotFoundException('La información no ha sido encontrada');
     }
     return response;    
+}
+async update(id: string, payload:UpdateProductDto){
+    const response = await  this.repository.findOneBy({id});
+    if (!response) {
+        throw new NotFoundException('La información no ha sido encontrada');
+    }
+    this.repository.merge(response, payload)
+    return this.repository.save(response);
+}
+async remove(id: string){
+    const response = await this.repository.findOneBy({id});
+    if(!response){
+        throw new NotFoundException('La información no ha sido encontrada');
+    }
+    return this.repository.softRemove(response);
+}
+async removeAll(payload: ProductEntity[]){
+    return this.repository.softRemove(payload);
+}
+
+private async paginateAndFilter(params: FilterProductDto){
+    let where: 
+    | FindOptionsWhere<ProductEntity>
+    | FindOptionsWhere<ProductEntity>[];
+    where = {};
+    let {page, search} = params;
+    const {limit} = params;
+    if (search) {
+        search = search.trim(),
+        page = 0;
+        where = [];
+        /*where.push({
+            name:ILike($%{search})
+        })*/
+    }
+    const data = this.repository.findAndCount({
+        relations: 
+    })
 }
 }
 
